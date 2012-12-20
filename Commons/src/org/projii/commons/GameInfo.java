85179ff3b28f241bb2d576c0fe8cdd2c9a7f2467
Yau Ref <yau.ref@gmail.com>
2012-12-20 20:35:35 +0400

    GameInfo is now can be serialized to bson

--- Commons/src/org/projii/commons/GameInfo.java
+++ Commons/src/org/projii/commons/GameInfo.java
@@ -1,10 +1,17 @@
 package org.projii.commons;
 
+import org.jai.BSON.BSONSerializable;
+
 public class GameInfo {
+    @BSONSerializable
     public final int gameId;
+    @BSONSerializable
     public final String serverIP;
+    @BSONSerializable
     public final String mapName;
+    @BSONSerializable
     public final int currentPlayersAmount;
+    @BSONSerializable
     public final int maxPlayersAmount;
 
     public GameInfo(int gameId, String serverIP, String mapName, int currentPlayersAmount, int maxPlayersAmount) {
