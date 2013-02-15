e531fb095af658ecff485cfa5856d853982973e2
Yau Ref <yau.ref@gmail.com>
2013-02-15 05:57:40 +0400

    Request and response interfaces

---
+++ Commons/src/org/projii/commons/net/Request.java
@@ -0,0 +1,5 @@
+package org.projii.commons.net;
+
+public interface Request {
+    int getType();
+}
---
+++ Commons/src/org/projii/commons/net/Response.java
@@ -0,0 +1,5 @@
+package org.projii.commons.net;
+
+public interface Response {
+    int getType();
+}
