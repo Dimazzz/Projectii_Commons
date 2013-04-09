3b404fc1a498f0f2ba12257808b859943415d227
Yau Ref <yau.ref@gmail.com>
2013-04-09 23:04:06 +0400

    Game server requests and responses now stored in Commons

---
+++ Commons/src/org/projii/commons/net/GameServerRequests.java
@@ -0,0 +1,11 @@
+package org.projii.commons.net;
+
+//Client requests to Game Server
+public final class GameServerRequests {
+    public static final int JOIN= 1;
+    public static final int LOGOUT = 5;
+    public static final int ERROR = 0;
+
+    private GameServerRequests() {
+    }
+}
---
+++ Commons/src/org/projii/commons/net/GameServerResponses.java
@@ -0,0 +1,9 @@
+package org.projii.commons.net;
+
+public final class GameServerResponses {
+    public static final int JOIN_RESULT = 1;
+    public static final int ERROR = 0;
+
+    private GameServerResponses() {
+    }
+}
