5d70739929aec98717476e1431dfc2e16c9a0573
SunShelbi <android92>
2013-05-10 22:39:13 +0400

    added new vars to ServerResponses

--- Commons/src/org/projii/commons/net/GameServerResponses.java
+++ Commons/src/org/projii/commons/net/GameServerResponses.java
@@ -3,6 +3,8 @@ package org.projii.commons.net;
 public final class GameServerResponses {
     public static final int JOIN_RESULT = 1;
     public static final int ERROR = 0;
+    public static final int MOVE_TO = 228;
+    public static final int FIRE_TO = 1488;
 
     private GameServerResponses() {
     }
